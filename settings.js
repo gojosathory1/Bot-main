fddef9dc02ab08cfe9df1f350be9a87d390c2964
gojosathory1 <maxsayura93@gmail.com>
2025-05-30 13:10:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=eFdzCSbY#dye5BjG3uRjo81YZzxltr2CsrQY45oZLC4Txzq3zBtI' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,  
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Cd9DDRTA#-rtrWjR28zouVKXr1SgsNtNecCeGwyC4fUs7f8i0gsc' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94743826406' : process.env.OWNER_NUMBER,  
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
-ALIVE:  process.env.ALIVE  || '> VAJIRA MD'  
+ALIVE:  process.env.ALIVE  || '> GOJO MD'  
 };
