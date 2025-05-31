e9a97b70209413e4fe5ed9c3743fc6d0481bc7fa
gojosathory1 <maxsayura93@gmail.com>
2025-05-31 07:55:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,5 +10,6 @@ SESSION_ID: process.env.SESSION_ID === undefined ? 'Cd9DDRTA#-rtrWjR28zouVKXr1Sg
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94743826406' : process.env.OWNER_NUMBER,  
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
-ALIVE:  process.env.ALIVE  || '> GOJO MD'  
+ALIVE:  process.env.ALIVE  || '> GOJO MD',
+GEMINI_API_KEY:process.env.GEMINI_API_KEY || 'AIzaSyDQIUl78aFtIgsNKY1RUU82nDkL905UbtA',
 };
